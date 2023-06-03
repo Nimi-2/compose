32ba850211beceb7d36e8076e924c13722ac54cf
Nimi-2 <emmadra20@gmail.com>
2023-06-03 19:58:56 +0200

    2nd

---
+++ nginx/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Strona startowa</title>
+</head>
+<body>
+    <h1>Witaj na stronie startowej nginx!</h1>
+    <?php
+    // Tutaj możesz umieścić kod PHP
+    // Na przykład: pobieranie danych z bazy danych, przetwarzanie danych itp.
+    ?>
+</body>
+</html>
